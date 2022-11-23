e1bf8d124114c5809b7dd523c7b4eaa0ce41e757
OpenUpDaze <toil_up@qq.com>
2022-11-23 22:08:57 +0800

    day 11 last

--- get_dataset.py
+++ get_dataset.py
@@ -1,5 +1,17 @@
+import torch
 import pandas as pd
+import matplotlib.pyplot as plt
 
 csv_path = 'housing.csv'
-data = pd.read_csv(csv_path)
-data.head()
+housing = pd.read_csv(csv_path)
+x_pd, y_pd = housing.iloc[:, -3:-2], housing.iloc[:, -2:-1]
+
+x = torch.tensor(x_pd.values)
+y = torch.tensor(y_pd.values)
+
+
+
+# x轴数据集中表头值如房价，纵轴表示这个值出现的次数
+housing.hist(bins=50,figsize=(20,15))
+plt.show()
+
